4fcab3c805e8af09299e56cdda8d02d25538aec5
root <root@gmail.com>
2023-05-02 14:49:06 +0530

    final project

--- asktopedia/settings.py
+++ asktopedia/settings.py
@@ -149,6 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
 ALLOWED_HOSTS = []
 
 # Default primary key field type
